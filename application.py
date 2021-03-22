894e805c1b4881874f4e65f72c07e4d108f58ef8
vahidseydi <seydi.vahid@gmail.com>
2021-03-22 03:04:41 +0000

    remove debug

--- application.py
+++ application.py
@@ -104,13 +104,12 @@ def upload():
 
     return None
 
-'''
+
 if __name__ == '__main__':
     # app.run(port=5002, debug=True)
 
     # Serve the app with gevent
     #http_server = WSGIServer(('', 5000), app)
     #http_server.serve_forever()
-    app.debug=True
+    #app.debug=True
     app.run()
-'''
